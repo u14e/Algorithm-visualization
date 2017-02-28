001fc2d777696be7634dfe302915f95b7de5ae8f
u14e <467030463@qq.com>
2017-03-01 00:32:36 +0800

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     context: path.resolve(__dirname, './src'),
     entry: {
-        app: './app.js',
+        app: ['babel-polyfill', './app.js'],
     },
     output: {
         path: path.resolve(__dirname, './dist'),
